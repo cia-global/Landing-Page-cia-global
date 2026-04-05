7cb3e1d2e0b4a1f87b6fd6628a760b67ed2b1c4b
davidmarint <maicolmarin0126@gmail.com>
2026-04-04 20:30:50 -0500

    feat: update BannerHomeIni styles for improved layout; enhance SectionCities with city links

--- src/components/banner/banner_home_ini.tsx
+++ src/components/banner/banner_home_ini.tsx
@@ -8,9 +8,9 @@
 export default function BannerHomeIni() {
   return (
     
-    <section className="relative bg-gray-50 text-white py-20 md:py-26 overflow-hidden">
+    <section className="relative bg-gray-50 text-white pt-24 md:pt-32  lg:py-20 overflow-hidden">
         <div className="absolute lg:top-0 lg:right-0 lg:w-1/2 lg:h-full lg:bg-gray-950 " />
-        <div className="absolute top-0 left-1/2 w-20 h-full bg-gradient-to-r from-gray-50 to-gray-950"></div>
+        <div className="hidden lg:block absolute top-0 left-1/2 w-20 h-full bg-gradient-to-r from-gray-50 to-gray-950"></div>
       <BackgroundElements />
         {/*  bg-gradient-to-br from-blue-600 via-blue-700 to-green-600 */}
       <div className="relative z-10  mx-auto">
@@ -24,7 +24,7 @@ export default function BannerHomeIni() {
                 src="/images/promocion.webp"
                 alt="50% descuento en comparendos"
                 fetchPriority="high"
-                className="w-72 sm:w-64 md:w-80 lg:w-80 xl:w-96 object-contain drop-shadow-2xl"
+                className="w-72 sm:w-72 md:w-80 lg:w-80 xl:w-96 object-contain drop-shadow-2xl"
               />
             </div>
             {/* <div className="bg-gray-900 backdrop-blur-md border border-white/20 rounded-3xl p-8 shadow-2xl"></div> */}
@@ -35,7 +35,7 @@ export default function BannerHomeIni() {
             </h1>
 
             {/* Descripción */}
-            <div className="px-16">
+            <div className="px-6 lg:px-2 xl:px-16">
             <p className="text-lg text-gray-700 leading-relaxed bg-gray-400/20 backdrop-blur-sm rounded-2xl p-5 border border-gray-400/70">
               Agenda hoy tu curso de seguridad vial y aprovecha los beneficios
               en la reducción del valor de comparendos de acuerdo con la{" "}
@@ -74,23 +74,23 @@ export default function BannerHomeIni() {
           </div>
 
           {/* ── DERECHA: Mapa de Colombia ── */}
-          <div className=" flex flex-col items-center xl:-mt-24 lg:-mt-44 sm:mt-12 bg-gray-950 lg:bg-gray-950/0">
+          <div className=" flex flex-col items-center xl:-mt-8 lg:-mt-8 sm:mt-12 bg-gray-950 lg:bg-gray-950/0">
               <div className="lg:hidden w-full h-8 bg-gradient-to-b from-gray-50 to-transparent z-10"></div>
 
-              <div className=" relative w-full max-w-xs sm:max-w-sm md:max-w-sm lg:max-w-xl h-[380px] sm:h-[460px] lg:h-[620px]">  
+              <div className=" relative w-full max-w-[22rem] sm:max-w-sm md:max-w-sm lg:max-w-xl h-[400px] sm:h-[460px] lg:h-[620px]">  
                 <div className=''><ColombiaMap/></div> 
               </div>
-              <div className="absolute bottom-4 left-[60%]">
+              <div className="absolute  bottom-4 left-3 md:left-6 lg:left-[60%] ">
               <div className="relative inline-block">
                 {/* Línea amarilla lateral */}
                 <div className="absolute left-0 top-1/2 -translate-y-1/2 w-1.5 h-4/5 bg-yellow-400 rounded-full"></div>
 
                 {/* Caja principal */}
-                <div className="bg-black border border-white rounded-2xl px-8 py-5 shadow-lg">
+                <div className="bg-black border border-white rounded-2xl px-6 sm:px-8 py-2 sm:py-5 shadow-lg">
                   <p className="text-gray-300 tracking-[0.2em] text-sm font-medium">
                     PRESENCIA NACIONAL
                   </p>
-                  <h2 className="text-yellow-400 text-2xl font-extrabold mt-1">
+                  <h2 className="text-yellow-400 text-md sm:text-2xl font-extrabold mt-1">
                     +25 SEDES ACTIVAS
                   </h2>
                 </div>
